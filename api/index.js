0a69b4f8680e168d5cbe1c08560552a6b9854e1c
mohamed-zizo-sketch <sci.mohamedabdelaziz01652@alexu.edu.eg>
2026-09-25 21:28:26 +0200

    fix: set outputDirectory to root for static assets and URL prefix in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,9 @@
 const app = require('../server.js');
 
-module.exports = app;
+module.exports = (req, res) => {
+  // Ensure req.url starts with /api for Express routing
+  if (!req.url.startsWith('/api')) {
+    req.url = '/api' + (req.url === '/' ? '' : req.url);
+  }
+  return app(req, res);
+};
